6f2a10011bd937b90fd780f541b4eeb7a370b89a
ezekg <ezekg@yahoo.com>
2015-02-16 15:58:46 -0600

    modify base of teejay class

--- lib/theme-juice/interactions/teejay.rb
+++ lib/theme-juice/interactions/teejay.rb
@@ -1,6 +1,16 @@
 # encoding: UTF-8
 
 module ThemeJuice
-    class Interaction::TeeJay# < ::ThemeJuice::Interaction
+    class Interaction::TeeJay
+
+        #
+        # Set up interactions and environment
+        #
+        # @return {Void}
+        #
+        def initialize
+            @environment = ::ThemeJuice::Environment
+            @interaction = ::ThemeJuice::Interaction
+        end
     end
 end
