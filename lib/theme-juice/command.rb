34369ed01df26b3ce95cc96de24a66864ad65f31
ezekg <ezekg@yahoo.com>
2015-04-20 15:09:18 -0500

    update how tasks are triggered

--- lib/theme-juice/command.rb
+++ lib/theme-juice/command.rb
@@ -22,20 +22,5 @@ module ThemeJuice
     def vm_location
       File.expand_path "#{@env.vm_path}/www/#{@env.vm_prefix}-#{@project.name}"
     end
-
-    #
-    # Run system commands
-    #
-    # @param {Array} commands
-    #   Array of commands to run
-    # @param {Bool}  silent   (true)
-    #   Silence all output from command
-    #
-    # @return {Void}
-    #
-    def run(commands, silent = true)
-      commands.map! { |cmd| "#{cmd.to_s} > /dev/null 2>&1" } if silent
-      system commands.join "&&"
-    end
   end
 end
--- lib/theme-juice/tasks/vm.rb
+++ lib/theme-juice/tasks/vm.rb
@@ -14,15 +14,24 @@ module ThemeJuice
 
       def execute
         @interact.log "Running method 'execute' for #{self.class.name}"
-        @tasks.each { |task| self.send "execute_#{task}" }
+        @tasks.each { |task| self.send task, :execute }
       end
 
       def unexecute
         @interact.log "Running method 'unexecute' for #{self.class.name}"
-        @tasks.reverse.each { |task| self.send "unexecute_#{task}" }
+        @tasks.reverse.each { |task| self.send task, :unexecute }
       end
 
       private
+
+      def path(action)
+        case action
+        when :execute
+          execute_path
+        when :unexcute
+          unexecute_path
+        end
+      end
 
       def execute_path
         @interact.log "Creating path"
