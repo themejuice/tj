b10e458e5ff2cf758774c389580b432314cf5c1b
ezekg <ezekg@yahoo.com>
2015-05-15 10:42:55 -0500

    update spec require path

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -1,7 +1,7 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
-lib = File.expand_path "../lib/", __FILE__
+lib = File.expand_path "../../lib/", __FILE__
 $:.unshift lib unless $:.include? lib
 
 require "fakefs/spec_helpers"
