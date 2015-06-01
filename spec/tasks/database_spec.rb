c19c4165d67c77c5e0fac44a336c0e74bb274744
ezekg <ezekg@yahoo.com>
2015-06-01 18:36:00 -0500

    add parenthesis

--- spec/tasks/database_spec.rb
+++ spec/tasks/database_spec.rb
@@ -5,7 +5,6 @@ describe ThemeJuice::Tasks::Database do
     @project = ThemeJuice::Project
 
     allow(@env).to receive(:vm_path).and_return File.expand_path("~/vagrant")
-    # allow(@env).to receive(:dryrun).and_return true
     allow(@project).to receive(:name).and_return "project"
     allow(@project).to receive(:db_host).and_return "project_db_host"
     allow(@project).to receive(:db_name).and_return "project_db_name"
@@ -24,13 +23,14 @@ describe ThemeJuice::Tasks::Database do
     
     it "should append project info to custom database file" do
       output = capture(:stdout) { @task.execute }
+      
       expect(File.binread(@sql_file)).to match /project_db_name/
       expect(File.binread(@sql_file)).to match /project_db_user/
       expect(File.binread(@sql_file)).to match /project_db_pass/
     end
     
     it "should expect custom database file to exist" do
-      expect(@task.send :entry_file_is_setup?).to be true
+      expect(@task.send(:entry_file_is_setup?)).to be true
     end
   end
 
@@ -38,6 +38,7 @@ describe ThemeJuice::Tasks::Database do
 
     it "should gsub project info from custom database file" do
       output = capture(:stdout) { @task.unexecute }
+      
       expect(File.binread(@sql_file)).not_to match /project_db_name/
       expect(File.binread(@sql_file)).not_to match /project_db_user/
       expect(File.binread(@sql_file)).not_to match /project_db_pass/
