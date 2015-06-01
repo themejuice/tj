36695e7692cf4c2fa1acdabd446484d6ff5add50
ezekg <ezekg@yahoo.com>
2015-06-01 18:16:33 -0500

    spec work

--- spec/tasks/database_spec.rb
+++ spec/tasks/database_spec.rb
@@ -7,24 +7,45 @@ describe ThemeJuice::Tasks::Database do
     allow(@env).to receive(:vm_path).and_return File.expand_path("~/vagrant")
     allow(@env).to receive(:dryrun).and_return true
     allow(@project).to receive(:name).and_return "project"
-    allow(@project).to receive(:db_host).and_return "db_host"
-    allow(@project).to receive(:db_name).and_return "db_name"
-    allow(@project).to receive(:db_user).and_return "db_user"
-    allow(@project).to receive(:db_pass).and_return "db_pass"
+    allow(@project).to receive(:db_host).and_return "project_db_host"
+    allow(@project).to receive(:db_name).and_return "project_db_name"
+    allow(@project).to receive(:db_user).and_return "project_db_user"
+    allow(@project).to receive(:db_pass).and_return "project_db_pass"
+    
+    FakeFS::FileSystem.clone "#{@env.vm_path}/database"
   end
 
   before :each do
-    @task = ThemeJuice::Tasks::Database.new
+    @task     = ThemeJuice::Tasks::Database.new
+    @sql_file = "#{@env.vm_path}/database/init-custom.sql"
   end
 
   describe "#execute" do
-    # Handled in entry_spec.rb
+    
+    it "should append project info to custom database file" do
+      output = capture(:stdout) { @task.execute }
+      expect(File.binread(@sql_file)).to match /project_db_host/
+      expect(File.binread(@sql_file)).to match /project_db_name/
+      expect(File.binread(@sql_file)).to match /project_db_user/
+      expect(File.binread(@sql_file)).to match /project_db_pass/
+      expect(output).to match /append/
+    end
+    
+    it "should expect custom database file to exist" do
+      output = capture(:stdout) { @task.execute }
+      expect(output).to match /create/
+    end
   end
 
   describe "#unexecute" do
 
-    it "should gsub custom database file" do
-      # Handled in entry_spec.rb
+    it "should gsub project info from custom database file" do
+      output = capture(:stdout) { @task.unexecute }
+      expect(File.binread(@sql_file)).not_to match /project_db_host/
+      expect(File.binread(@sql_file)).not_to match /project_db_name/
+      expect(File.binread(@sql_file)).not_to match /project_db_user/
+      expect(File.binread(@sql_file)).not_to match /project_db_pass/
+      expect(output).to match /gsub/
     end
 
     context "when Project#db_drop is set to true" do
