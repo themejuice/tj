31d3c85d4b52949f830becab89794b979b7eadcc
ezekg <ezekg@yahoo.com>
2015-06-10 17:06:49 -0500

    add automatic host port forwarding on osx

--- lib/theme-juice/tasks/forward_ports.rb
+++ lib/theme-juice/tasks/forward_ports.rb
@@ -18,13 +18,34 @@ module ThemeJuice
       def execute
         create_entry_file
         create_entry do
-%Q{config.vm.network "forwarded_port", guest: 80,  host: 8080
+%Q{#{forward_host_ports}
+config.vm.network "forwarded_port", guest: 80,  host: 8080
 config.vm.network "forwarded_port", guest: 443, host: 8443}
         end
       end
 
       def unexecute
         remove_entry
+      end
+
+      private
+
+      # @todo I'd like to eventually support every OS. I'm only familiar with
+      #  OSX, so that's why there's nothing else here. Pull requests welcome!
+      def forward_host_ports
+        if OS.osx?
+          %Q{if defined? VagrantPlugins::Triggers
+  config.trigger.after [:up, :reload, :provision], :stdout => true do
+    system 'echo "
+rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 8080
+rdr pass inet proto tcp from any to any port 443 -> 127.0.0.1 port 8443
+" | sudo pfctl -ef - >/dev/null 2>&1; echo "Forwarding ports (80 => 8080)\\nForwarding ports (443 => 8443)"'
+  end
+  config.trigger.after [:halt, :suspend, :destroy], :stdout => true do
+    system 'sudo pfctl -F all -f /etc/pf.conf >/dev/null 2>&1; echo "Removing forwarded ports (80 => 8080)\\nRemoving forwarded ports (443 => 8443)"'
+  end
+end}
+        end
       end
     end
   end
