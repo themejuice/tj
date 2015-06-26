effa1e523d04f33f98de0775161d872d4110f93a
ezekg <ezekg@yahoo.com>
2015-06-26 17:09:57 -0500

    update repo deploy task to accept at options

--- lib/theme-juice/tasks/deploy/repo.rb
+++ lib/theme-juice/tasks/deploy/repo.rb
@@ -18,9 +18,9 @@ module ThemeJuice
         def configure_scm
           @io.log "Configuring repository"
 
-          @env.cap.config.set :repo_url, @config.deployment.repository.url
-          @env.cap.config.set :branch,   @config.deployment.repository.branch
-          @env.cap.config.set :scm,      @config.deployment.repository.scm
+          @config.deployment.repository.each do |_, (key, value)|
+            @env.cap.config.set :"#{key}", value
+          end
         end
       end
     end
