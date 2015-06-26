8af01c2bdb02e1d10bf18a5747171c70d78b40e6
ezekg <ezekg@yahoo.com>
2015-06-26 10:07:59 -0500

    add hash to open struct method

--- lib/theme-juice/helpers/hash_helper.rb
+++ lib/theme-juice/helpers/hash_helper.rb
@@ -1,10 +1,24 @@
+require "ostruct"
+
 module ThemeJuice
   module HashHelper
+
     def symbolize_keys
-      inject({}) do |acc, (k,v)|
+      inject({}) do |acc, (k, v)|
         key = String === k ? k.to_sym : k
         value = Hash === v ? v.symbolize_keys : v
         acc[key] = value
+        acc
+      end
+    end
+
+    def to_ostruct(acc = self)
+      case acc
+      when Hash
+        OpenStruct.new Hash[acc.map { |k, v| [k, to_ostruct(v)] } ]
+      when Array
+        acc.map { |x| to_ostruct(x) }
+      else
         acc
       end
     end
