daef1b7fdb02b105de5cb64ff67bc9229f44bad2
ezekg <ezekg@yahoo.com>
2015-06-27 03:34:00 +0000

    catch errors related to invalid deploy config

--- lib/theme-juice/tasks/settings.rb
+++ lib/theme-juice/tasks/settings.rb
@@ -7,8 +7,6 @@ module ThemeJuice
 
       def initialize
         super
-
-        @settings = @config.deployment
       end
 
       def execute
@@ -20,11 +18,17 @@ module ThemeJuice
       def configure_settings
         @io.log "Configuring Capistrano"
 
-        set :application, @settings.application.name
-        set :rsync_options, @settings.rsync_options rescue nil
-        %w[settings repository].each do |task|
-          @settings.send(task).symbolize_keys.each do |key, value|
-            set key, value
+        begin
+          set :application, @config.deployment.application.name
+          set :rsync_options, @config.deployment.rsync_options
+          %w[settings repository].each do |task|
+            @config.deployment.send(task).symbolize_keys.each do |key, value|
+              set key, value
+            end
+          end
+        rescue NoMethodError => err
+          @io.error "Oops! It looks like you're missing a few deployment settings" do
+            puts err
           end
         end
       end
