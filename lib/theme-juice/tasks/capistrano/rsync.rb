75c2d8f20d9ee6c800452daaeca38f23d7414318
ezekg <ezekg@yahoo.com>
2015-06-29 10:18:31 -0500

    add rsync:set_current_revision fix

---
+++ lib/theme-juice/tasks/capistrano/rsync.rb
@@ -0,0 +1,12 @@
+namespace :rsync do
+
+  # @see https://github.com/moll/capistrano-rsync/issues/15
+  task :set_current_revision do
+    run_locally do
+      within fetch(:rsync_stage) do
+        rev = capture :git, "rev-parse", "HEAD"
+        set :current_revision, rev
+      end
+    end
+  end
+end
--- lib/theme-juice/tasks/load.rb
+++ lib/theme-juice/tasks/load.rb
@@ -30,7 +30,7 @@ module ThemeJuice
         require "capistrano/framework"
 
         tasks_dir = "#{File.dirname(__FILE__)}/capistrano"
-        tasks     = %w[db uploads deploy]
+        tasks     = %w[db uploads deploy rsync]
 
         tasks.each { |task| load "#{tasks_dir}/#{task}.rb" }
       end
