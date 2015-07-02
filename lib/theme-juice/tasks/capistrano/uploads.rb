e106df667ae7d697c6f45991184a7861f40ba75e
ezekg <ezekg@yahoo.com>
2015-07-02 13:32:05 -0500

    revert last change

--- lib/theme-juice/tasks/capistrano/uploads.rb
+++ lib/theme-juice/tasks/capistrano/uploads.rb
@@ -4,11 +4,6 @@ namespace :uploads do
 
   desc "Push local uploads to remote"
   task :push do
-
-    unless fetch(:uploads_dir).end_with? "/"
-      set :uploads_dir, fetch(:uploads_dir) << "/"
-    end
-
     on roles(:app) do
       upload! fetch(:vm_uploads_dir), fetch(:uploads_dir), {
         recursive: true
@@ -18,11 +13,6 @@ namespace :uploads do
 
   desc "Pull remote uploads to local"
   task :pull do
-
-    unless fetch(:vm_uploads_dir).end_with? "/"
-      set :vm_uploads_dir, fetch(:vm_uploads_dir) << "/"
-    end
-
     on roles(:app) do
       download! fetch(:uploads_dir), fetch(:vm_uploads_dir), {
         recursive: true
