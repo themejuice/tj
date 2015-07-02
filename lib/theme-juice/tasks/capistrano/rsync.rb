4f2d4d717394160c2e878473b0674c1105dcf6fb
ezekg <ezekg@yahoo.com>
2015-07-02 11:17:54 -0500

    allow files to be excluded per-stage. close #38

--- lib/theme-juice/tasks/capistrano/rsync.rb
+++ lib/theme-juice/tasks/capistrano/rsync.rb
@@ -1,26 +1,42 @@
 # encoding: UTF-8
 
 namespace :rsync do
-  task :stage
 
   after :stage, :precompile do
-    within fetch(:rsync_stage) do
-      fetch(:rsync_install, []).each { |t| system t }
+    run_locally do
+      within fetch(:rsync_stage) do
+        fetch(:rsync_install, []).each { |t| execute t }
+      end
     end
   end
 
   after :precompile, :ignore do
-    within fetch(:rsync_stage) do
-      fetch(:rsync_ignore, []).each { |f| system "rm #{f}" }
+    run_locally do
+      within fetch(:rsync_stage) do
+        fetch(:rsync_ignore, []).each { |f| execute :rm, f }
+      end
     end
   end
 
-  # @see https://github.com/moll/capistrano-rsync/issues/15
-  task :set_current_revision do
+  after "deploy:finished", :clean do
     run_locally do
-      within fetch(:rsync_stage) do
-        rev = capture :git, "rev-parse", "HEAD"
-        set :current_revision, rev
+      execute :rm, "-rf", fetch(:rsync_stage)
+    end
+  end
+
+  namespace :prepare do
+
+    before "deploy:check:directories", :directories do
+      on roles(:app) do
+        within fetch(:deploy_to) do
+          execute :mkdir, "-p", fetch(:rsync_cache)
+        end
+      end
+    end
+
+    before "deploy:check:linked_files", :files do
+      on roles(:app) do
+        fetch(:linked_files).each { |f| execute :touch, shared_path.join(f) }
       end
     end
   end
