e6438603d78c83fe8fe1defdd09f891a4f107200
ezekg <ezekg@yahoo.com>
2015-08-18 08:40:20 -0500

    remove redundant assignment

--- lib/theme-juice/env.rb
+++ lib/theme-juice/env.rb
@@ -24,67 +24,67 @@ module ThemeJuice
     attr_accessor :cap
 
     def vm_box=(val)
-      @vm_box = val ||= ENV.fetch("TJ_VM_BOX") { "git@github.com:ezekg/theme-juice-vvv.git" }
+      @vm_box = val || ENV.fetch("TJ_VM_BOX") { "git@github.com:ezekg/theme-juice-vvv.git" }
     end
 
     def vm_path=(val)
-      @vm_path = val ||= ENV.fetch("TJ_VM_PATH") { File.expand_path("~/vagrant") }
+      @vm_path = val || ENV.fetch("TJ_VM_PATH") { File.expand_path("~/vagrant") }
     end
 
     def vm_ip=(val)
-      @vm_ip = val ||= ENV.fetch("TJ_VM_IP") { "192.168.50.4" }
+      @vm_ip = val || ENV.fetch("TJ_VM_IP") { "192.168.50.4" }
     end
 
     def vm_prefix=(val)
-      @vm_prefix = val ||= ENV.fetch("TJ_VM_PREFIX") { "tj-" }
+      @vm_prefix = val || ENV.fetch("TJ_VM_PREFIX") { "tj-" }
     end
 
     def yolo=(val)
-      @yolo = val ||= ENV.fetch("TJ_YOLO") { false }
+      @yolo = val || ENV.fetch("TJ_YOLO") { false }
     end
 
     def boring=(val)
-      @boring = val ||= ENV.fetch("TJ_BORING") { false }
+      @boring = val || ENV.fetch("TJ_BORING") { false }
     end
 
     def no_unicode=(val)
-      @no_unicode = val ||= ENV.fetch("TJ_NO_UNICODE") { boring }
+      @no_unicode = val || ENV.fetch("TJ_NO_UNICODE") { boring }
     end
 
     def no_colors=(val)
-      @no_colors = val ||= ENV.fetch("TJ_NO_COLORS") { boring }
+      @no_colors = val || ENV.fetch("TJ_NO_COLORS") { boring }
     end
 
     def no_animations=(val)
-      @no_animations = val ||= ENV.fetch("TJ_NO_ANIMATIONS") { boring }
+      @no_animations = val || ENV.fetch("TJ_NO_ANIMATIONS") { boring }
     end
 
     def no_landrush=(val)
-      @no_landrush = val ||= ENV.fetch("TJ_NO_LANDRUSH") { false }
+      @no_landrush = val || ENV.fetch("TJ_NO_LANDRUSH") { false }
     end
 
     def no_port_forward=(val)
-      @no_port_forward = val ||= ENV.fetch("TJ_NO_PORT_FORWARD") { false }
+      @no_port_forward = val || ENV.fetch("TJ_NO_PORT_FORWARD") { false }
     end
 
     def verbose=(val)
-      @verbose = val ||= ENV.fetch("TJ_VERBOSE") { false }
+      @verbose = val || ENV.fetch("TJ_VERBOSE") { false }
     end
 
     def quiet=(val)
-      @quiet = val ||= ENV.fetch("TJ_QUIET") { false }
+      @quiet = val || ENV.fetch("TJ_QUIET") { false }
     end
 
     def trace=(val)
-      @trace = val ||= ENV.fetch("TJ_TRACE") { false }
+      @trace = val || ENV.fetch("TJ_TRACE") { false }
     end
 
     def dryrun=(val)
-      @dryrun = val ||= ENV.fetch("TJ_DRYRUN") { false }
+      @dryrun = val || ENV.fetch("TJ_DRYRUN") { false }
     end
 
     def nginx=(val)
-      @nginx = val ||= ENV.fetch("TJ_NGINX") { false }
+      @nginx = val || ENV.fetch("TJ_NGINX") { false }
     end
 
     extend self
