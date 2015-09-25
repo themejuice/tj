fa56dd09d0bababd4f8fde57ebf8eb5e403914e2
ezekg <ezekg@yahoo.com>
2015-09-25 09:06:08 -0500

    ensure path exists before file push/pull

--- lib/theme-juice/tasks/capistrano/file.rb
+++ lib/theme-juice/tasks/capistrano/file.rb
@@ -6,6 +6,11 @@ namespace :file do
   task :push, [:file] do |t, args|
     on roles(:app) do
       if File.exist? args[:file]
+
+        within release_path do
+          execute :mkdir, "-p", File.dirname(args[:file])
+        end
+
         upload! args[:file], release_path.join(args[:file])
       else
         error "Could not locate local file '#{args[:file]}'"
@@ -16,6 +21,11 @@ namespace :file do
   desc "Pull file from remote"
   task :pull, [:file] do |t, args|
     on roles(:app) do
+
+      run_locally do
+        execute :mkdir, "-p", File.dirname(args[:file])
+      end
+
       download! release_path.join(args[:file]), args[:file]
     end
   end
