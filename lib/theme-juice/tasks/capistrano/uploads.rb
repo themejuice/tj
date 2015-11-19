6bae11152bf08767617a3ead455e23ec300f82bd
ezekg <ezekg@yahoo.com>
2015-11-19 12:04:04 -0600

    remove commented lines

--- lib/theme-juice/tasks/capistrano/uploads.rb
+++ lib/theme-juice/tasks/capistrano/uploads.rb
@@ -5,20 +5,10 @@ namespace :uploads do
   desc "Push local uploads to remote"
   task :push do
     invoke "dir:push", fetch(:uploads_dir)
-    # on roles(:app) do
-    #   upload! fetch(:vm_uploads_dir), shared_path.join(Pathname.new(fetch(:uploads_dir)).parent), {
-    #     recursive: true
-    #   }
-    # end
   end
 
   desc "Pull remote uploads to local"
   task :pull do
     invoke "dir:pull", fetch(:uploads_dir)
-    # on roles(:app) do
-    #   download! shared_path.join(fetch(:uploads_dir)), Pathname.new(fetch(:vm_uploads_dir)).parent, {
-    #     recursive: true
-    #   }
-    # end
   end
 end
