83af668fd32a81f3c977f9c87103090664e5855b
ezekg <ezekg@yahoo.com>
2016-02-05 12:23:47 -0600

    add more tests for vm-box task

--- spec/tasks/vm_box_spec.rb
+++ spec/tasks/vm_box_spec.rb
@@ -2,7 +2,7 @@ describe ThemeJuice::Tasks::VMBox do
 
   before do
     @env = ThemeJuice::Env
-    
+
     allow(@env).to receive(:vm_path).and_return File.expand_path("~/tj-vagrant-test")
     allow(@env).to receive(:vm_box).and_return "git@github.com:some/vagrant/box.git"
     allow(@env).to receive(:verbose).and_return true
@@ -14,12 +14,32 @@ describe ThemeJuice::Tasks::VMBox do
   end
 
   describe "#execute" do
-    it "should clone vagrant box to vm path" do
-      output = capture(:stdout) { @task.execute }
-      
-      expect(output).to match /git clone/
-      expect(output).to match /#{@env.vm_box}/
-      expect(output).to match /#{@env.vm_path}/
+
+    context "when a vagrant box is not detected" do
+
+      it "should clone vagrant box to vm path" do
+        output = capture(:stdout) { @task.execute }
+
+        expect(output).to match /git clone/
+        expect(output).to match /#{@env.vm_box}/
+        expect(output).to match /#{@env.vm_path}/
+      end
+    end
+
+    context "when a vagrant box is detected" do
+
+      before do
+        FileUtils.mkdir_p "#{@env.vm_path}"
+        FileUtils.touch "#{@env.vm_path}/Vagrantfile"
+      end
+
+      it "should not clone vagrant box to vm path" do
+        output = capture(:stdout) { @task.execute }
+
+        expect(output).to_not match /git clone/
+        expect(output).to_not match /#{@env.vm_box}/
+        expect(output).to_not match /#{@env.vm_path}/
+      end
     end
   end
 end
