9a2246ea3d73dbf12e0b632f4692a7fe58eb9128
ezekg <ezekg@yahoo.com>
2016-08-15 10:40:26 -0500

    add temp spec for update command

---
+++ spec/commands/update_spec.rb
@@ -0,0 +1,12 @@
+describe ThemeJuice::Commands::Update do
+
+  before do
+    @update = ThemeJuice::Commands::Update
+  end
+
+  describe "#execute" do
+    it "should successfully update" do
+      expect(@update.new).to be
+    end
+  end
+end
